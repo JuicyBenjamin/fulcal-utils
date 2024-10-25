bdb582ba1e4d5f2d4557a461053efa7e62d0c886
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-10-25 11:19:55 +0200

    feat: add useFullCalendar util

--- main.ts
+++ main.ts
@@ -1,8 +1,55 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import type { CalendarApi } from 'npm:@fullcalendar/core'
+import type FullCalendar from 'npm:@fullcalendar/react'
+import { create } from 'npm:zustand'
+
+type TUseFullCalendar = {
+  calendars: Record<string, FullCalendar>
+  setCalendar: ({
+    id,
+    calendar,
+  }: {
+    id?: string
+    calendar: FullCalendar
+  }) => void
+  getApi: (id?: string) => CalendarApi | undefined
+  titles: Record<string, string>
+  getTitle: (id?: string) => string | undefined
+  setTitle: ({ title, id }: { title?: string; id?: string }) => void
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const defaultId = 'default'
+
+export const useFullCalendar = create<TUseFullCalendar>((set, get) => ({
+  calendars: {},
+
+  setCalendar: ({ id = defaultId, calendar }) =>
+    set((state) => ({
+      calendars: {
+        ...state.calendars,
+        [id]: calendar,
+      },
+    })),
+
+  getApi: (id = defaultId) => {
+    const calendarApi = get().calendars[id]?.getApi()
+    if (calendarApi) {
+      return calendarApi
+    } else {
+      return undefined
+    }
+  },
+  titles: {},
+  getTitle: (id = defaultId) => {
+    const title = get().titles[id]
+    if (title == null) {
+      const calendarApi = get().calendars[id]?.getApi()
+      if (calendarApi) {
+        return calendarApi.view.title
+      } else {
+        return undefined
+      }
+    }
+    return title
+  },
+  setTitle: ({ id = defaultId, title }) => set({ [id]: title }),
+}))
